7b7b223318d739e5244647fe474f2b208fb4ee94
Shubham Jain <shubhamj2604@gmail.com>
2021-06-11 01:06:42 +0000

    Added setup.py

--- excel-compare/__init__.py
+++ excel-compare/__init__.py
@@ -1,7 +1,1 @@
 from excel_compare import CompareFiles
-
-if __name__ == '__main__':
-    file_one = '../test files/similar_excel_files/one.xlsx'
-    file_two = '../test files/similar_excel_files/two.xlsx'
-    compare_files = CompareFiles(file_one, file_two)
-    compare_files.compare_excel()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A CLI based Python application to compare excel files'
+LONG_DESCRIPTION = 'A CLI based Python application to compare files like csv, xlsx'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="excel-compare",
+    version=VERSION,
+    author="Shubham Jain",
+    author_email="shubhamj.code@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas', 'xlrd', 'openpyxl'],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['excel', 'compare', 'excel-compare'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
